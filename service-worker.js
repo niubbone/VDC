f58ba46e504a7ae343c4804328be454a41a4edb4
niubbone <guren@tiscali.it>
2026-01-05 09:17:33 +0100

    Update service worker version to 4.0.1

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 //  SERVICE WORKER - VECCHIA DOGANA - VERSIONE OTTIMIZZATA
 //  Cambia SOLO questo numero ad ogni aggiornamento ↓
 // ═══════════════════════════════════════════════════════════════════
-const VERSION = '4.0.0';
+const VERSION = '4.0.1';
 // ═══════════════════════════════════════════════════════════════════
 
 // ═══════════════════════════════════════════════════════════════════
